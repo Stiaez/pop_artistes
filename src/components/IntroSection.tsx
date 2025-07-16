e640a6d236cdc9ab1465715f26e3d26a7f493566
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2025-07-17 01:29:15 +0200

    add: interval that switch image

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Typography } from '@mui/material';
 import { motion } from 'framer-motion';
@@ -9,19 +10,45 @@ const MotionTypography = motion(Typography);
 export default function IntroSection() {
     const { t } = useTranslation();
 
+    const [mobileImageIndex, setMobileImageIndex] = useState(1);
+
+    const [desktopImageIndex, setDesktopImageIndex] = useState(1);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setMobileImageIndex((prev) => (prev % 3) + 1);
+            setDesktopImageIndex((prev) => (prev % 3) + 1);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <section className="min-h-screen grid grid-cols-1 md:grid-cols-2">
-            <div className="relative md:sticky top-0 h-screen hidden md:block bg-[#292727]">
+            <div className="md:hidden w-full">
                 <img
-                    src="/white_paper/1.jpg"
-                    alt="White Paper"
-                    className="object-contain h-full w-full p-10"
+                    src={`/white_paper/white_paper_${mobileImageIndex}.jpg`}
+                    alt={`White Paper Mobile ${mobileImageIndex}`}
+                    className="object-contain w-full"
+                />
+            </div>
+
+            <div className="relative md:sticky top-0 h-screen hidden md:flex flex-col bg-[#292727] overflow-y-auto">
+                <img
+                    src="/white_paper/cover_page.jpg"
+                    alt="White Paper Cover"
+                    className="object-contain w-full"
+                />
+                <img
+                    src={`/white_paper/book_open_${desktopImageIndex}.jpg`}
+                    alt={`White Paper Page ${desktopImageIndex}`}
+                    className="object-contain w-full"
                 />
             </div>
 
             <div className="overflow-y-auto px-6 py-10 text-white max-h-screen text-center">
                 <MotionTypography
-                    className="title"
+                    className="title mb-10"
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ delay: 0.2 }}
@@ -30,34 +57,70 @@ export default function IntroSection() {
                 </MotionTypography>
 
                 <div className="mt-6">
-                    <MotionTypography className="text" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4 }}>
+                    <MotionTypography
+                        className="text"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.4 }}
+                    >
                         {t('descriptionPartOne')}
                     </MotionTypography>
                 </div>
 
-                <MotionTypography className="text mt-6" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.6}}>
-                    {t('descriptionPartTwo')}
-                </MotionTypography>
+                <div className="mt-6">
+                    <MotionTypography
+                        className="text"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.6 }}
+                    >
+                        {t('descriptionPartTwo')}
+                    </MotionTypography>
+                </div>
 
-                <div className="mt-4 mb-6">
-                    <MotionTypography className="text-bold mt-6" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.8}}>
+                <div className="mt-6">
+                    <MotionTypography
+                        className="text-bold"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.8 }}
+                    >
                         {t('descriptionPartThree')}
                     </MotionTypography>
                 </div>
 
-                <MotionTypography className="text" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.0 }}>
-                    {t('descriptionPartFour')}
-                </MotionTypography>
+                <div className="mt-4">
+                    <MotionTypography
+                        className="text"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1.0 }}
+                    >
+                        {t('descriptionPartFour')}
+                    </MotionTypography>
+                </div>
 
-                <div className="mt-2 mb-2">
-                    <MotionTypography className="text" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1.2}}>
+                <div className="mt-4">
+                    <MotionTypography
+                        className="text"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1.2 }}
+                    >
                         {t('descriptionPartFive')}
                     </MotionTypography>
                 </div>
 
-                <MotionTypography className="text" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.4 }}>
-                    {t('descriptionEnd')}
-                </MotionTypography>
+                <div className="mt-4 mb-8">
+                    <MotionTypography
+                        className="text"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1.4 }}
+                    >
+                        {t('descriptionEnd')}
+                    </MotionTypography>
+                </div>
 
                 <DownloadForm />
             </div>
